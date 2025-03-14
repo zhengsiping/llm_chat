2b27a86863537a73a5b56cd5b9ae5e1d4c25ecc8
Siping Zheng <sipzheng@linkedin.com>
2025-03-14 03:20:54 -0700

    add production

--- server/main.py
+++ server/main.py
@@ -15,6 +15,8 @@ app = FastAPI()
 # Define allowed origins
 origins = [
     "http://localhost:5174",
+    "https://localhost:5173",
+    "https://localhost:5174",
     "http://localhost:3000",  # React/Vue/Angular development
     "http://118.178.91.36/",     # Allow specific domain
     "https://118.178.91.36/",     # Allow specific domain
@@ -28,6 +30,16 @@ app.add_middleware(
     allow_methods=["*"],  # Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
     allow_headers=["*"],  # Allow all headers
 )
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=8000,
+        ssl_keyfile="./certs/key.pem",
+        ssl_certfile="./certs/cert.pem"
+    )
 
 # Include the router
 app.include_router(query.router)
